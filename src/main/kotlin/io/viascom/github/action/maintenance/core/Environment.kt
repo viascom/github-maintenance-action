2d17bc6ed630ab478872297b51c5178be36979ed
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-11-18 01:29:12 +0100

    clean: code review changes

--- src/main/kotlin/io/viascom/github/action/maintenance/core/Environment.kt
+++ src/main/kotlin/io/viascom/github/action/maintenance/core/Environment.kt
@@ -3,7 +3,7 @@ package io.viascom.github.action.maintenance.core
 object Environment {
     val gitHubToken: String = System.getenv("INPUT_GITHUB_TOKEN")
     val githubBaseUrl: String = System.getenv("INPUT_GITHUB_BASE_URL")
-    val repository: String = System.getenv("INPUT_GITHUB_REPOSITORY")
+    val repository: String = System.getenv("INPUT_REPOSITORY")
     val retentionDays = System.getenv("INPUT_RETENTION_DAYS").toLong()
     val deleteLogs = System.getenv("INPUT_DELETE_LOGS").toBoolean()
     val deleteArtifacts = System.getenv("INPUT_DELETE_ARTIFACTS").toBoolean()
